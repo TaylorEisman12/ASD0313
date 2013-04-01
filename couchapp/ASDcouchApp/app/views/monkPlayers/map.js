30264801e494de8c661ac75984618c28ebe8e379
Taylor Eisman <TaylorEisman@fullsail.edu>
2013-04-01 13:19:41 -0500

    test cloudant link not workin

--- couchapp/ASDcouchApp/app/views/monkPlayers/map.js
+++ couchapp/ASDcouchApp/app/views/monkPlayers/map.js
@@ -1,11 +1,11 @@
-function(doc) {
-  if (doc._id.substr(0,4) === "monk") {
-    emit(doc._id, {
-    	"playerName": doc.playerName,
-    	"playerClass": doc.playerClass,
-    	"level": doc.level,
-    	"hardcore": doc.hardcore
-    	"difficulty": doc.difficulty
-    });
-  }
+function (doc) {
+	if(doc._id.substr(0,4) === "monk") {
+		emit(doc._id, {
+			"playerName": doc.playerName,
+			"playerClass": doc.playerClass,
+			"level": doc.level,
+			"hardcore": doc.hardcore,
+			"difficulty": doc.difficulty
+		});
+	}
 };
